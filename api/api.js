8d81ea5dba3c310d47a36be6b6e757fa0aa13d43
lixufeng <44545545>
2017-07-12 14:40:42 +0800

    123

--- api/api.js
+++ api/api.js
@@ -67,12 +67,9 @@ exports.API = {
     },
 
     //显示用户注册页面
-    "_user/showRegister":{
-        method:'POST',
-        postMustParams:{
-            IS_OPEN_SHARE   :''
-        }
-
+    "/_user/showRegister":{
+        method:'GET',
+        getMustParams:{}
     },
 
 
